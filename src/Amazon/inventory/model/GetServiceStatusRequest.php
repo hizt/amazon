d3ab476018cd80ed29671055f29ca669ed90da89
abel-wang <Abelwang@glitzhome.com>
2018-01-10 17:47:49 +0800

    inventory client

---
+++ src/Amazon/inventory/model/GetServiceStatusRequest.php
@@ -0,0 +1,188 @@
+<?php
+/*******************************************************************************
+ * Copyright 2009-2016 Amazon Services. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * You may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at: http://aws.amazon.com/apache2.0
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ *******************************************************************************
+ * PHP Version 5
+ * @category Amazon
+ * @package  FBA Inventory Service MWS
+ * @version  2010-10-01
+ * Library Version: 2014-09-30
+ * Generated: Wed May 04 17:14:15 UTC 2016
+ */
+
+namespace inventory\model;
+
+use inventory\Model;
+
+
+/**
+ * GetServiceStatusRequest
+ * 
+ * Properties:
+ * <ul>
+ * 
+ * <li>SellerId: string</li>
+ * <li>MWSAuthToken: string</li>
+ * <li>Marketplace: string</li>
+ *
+ * </ul>
+ */
+
+ class GetServiceStatusRequest extends Model {
+
+    public function __construct($data = null)
+    {
+        $this->_fields = array (
+        'SellerId' => array('FieldValue' => null, 'FieldType' => 'string'),
+        'MWSAuthToken' => array('FieldValue' => null, 'FieldType' => 'string'),
+        'Marketplace' => array('FieldValue' => null, 'FieldType' => 'string'),
+        );
+        parent::__construct($data);
+    }
+
+    /**
+     * Get the value of the SellerId property.
+     *
+     * @return String SellerId.
+     */
+    public function getSellerId()
+    {
+        return $this->_fields['SellerId']['FieldValue'];
+    }
+
+    /**
+     * Set the value of the SellerId property.
+     *
+     * @param string $value sellerId
+     * @return $this instance
+     */
+    public function setSellerId($value)
+    {
+        $this->_fields['SellerId']['FieldValue'] = $value;
+        return $this;
+    }
+
+    /**
+     * Check to see if SellerId is set.
+     *
+     * @return true if SellerId is set.
+     */
+    public function isSetSellerId()
+    {
+                return !is_null($this->_fields['SellerId']['FieldValue']);
+            }
+
+    /**
+     * Set the value of SellerId, return this.
+     *
+     * @param sellerId
+     *             The new value to set.
+     *
+     * @return $this instance.
+     */
+    public function withSellerId($value)
+    {
+        $this->setSellerId($value);
+        return $this;
+    }
+
+    /**
+     * Get the value of the MWSAuthToken property.
+     *
+     * @return String MWSAuthToken.
+     */
+    public function getMWSAuthToken()
+    {
+        return $this->_fields['MWSAuthToken']['FieldValue'];
+    }
+
+    /**
+     * Set the value of the MWSAuthToken property.
+     *
+     * @param $value string MWSAuthToken
+     * @return $this instance
+     */
+    public function setMWSAuthToken($value)
+    {
+        $this->_fields['MWSAuthToken']['FieldValue'] = $value;
+        return $this;
+    }
+
+    /**
+     * Check to see if MWSAuthToken is set.
+     *
+     * @return true if MWSAuthToken is set.
+     */
+    public function isSetMWSAuthToken()
+    {
+        return !is_null($this->_fields['MWSAuthToken']['FieldValue']);
+    }
+
+    /**
+     * Set the value of MWSAuthToken, return this.
+     *
+     * @param mwsAuthToken
+     *             The new value to set.
+     *
+     * @return $this instance.
+     */
+    public function withMWSAuthToken($value)
+    {
+        $this->setMWSAuthToken($value);
+        return $this;
+    }
+
+    /**
+     * Get the value of the Marketplace property.
+     *
+     * @return String Marketplace.
+     */
+    public function getMarketplace()
+    {
+        return $this->_fields['Marketplace']['FieldValue'];
+    }
+
+    /**
+     * Set the value of the Marketplace property.
+     *
+     * @param  marketplace string
+     * @return $this instance
+     */
+    public function setMarketplace($value)
+    {
+        $this->_fields['Marketplace']['FieldValue'] = $value;
+        return $this;
+    }
+
+    /**
+     * Check to see if Marketplace is set.
+     *
+     * @return true if Marketplace is set.
+     */
+    public function isSetMarketplace()
+    {
+                return !is_null($this->_fields['Marketplace']['FieldValue']);
+            }
+
+    /**
+     * Set the value of Marketplace, return this.
+     *
+     * @param marketplace
+     *             The new value to set.
+     *
+     * @return $this instance.
+     */
+    public function withMarketplace($value)
+    {
+        $this->setMarketplace($value);
+        return $this;
+    }
+
+}
