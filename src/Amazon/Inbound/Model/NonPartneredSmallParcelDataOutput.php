52ca53ae3d25da636c1f9b7c4e8451990b2615e5
abel-wang <Abelwang@glitzhome.com>
2018-03-06 17:01:04 +0800

    amazon wms inbound api

---
+++ src/Amazon/Inbound/Model/NonPartneredSmallParcelDataOutput.php
@@ -0,0 +1,92 @@
+<?php
+/*******************************************************************************
+ * Copyright 2009-2016 Amazon Services. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at: http://aws.amazon.com/apache2.0
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *******************************************************************************
+ * PHP Version 5
+ * @category Amazon
+ * @package  FBA Inbound Service MWS
+ * @version  2010-10-01
+ * Library Version: 2016-10-05
+ * Generated: Wed Oct 05 06:15:45 PDT 2016
+ */
+namespace Amazon\Inbound\Model;
+
+use Amazon\Inbound\Model;
+
+
+
+/**
+ * FBAInboundServiceMWS_Model_NonPartneredSmallParcelDataOutput
+ *
+ * Properties:
+ * <ul>
+ *
+ * <li>PackageList: FBAInboundServiceMWS_Model_NonPartneredSmallParcelPackageOutputList</li>
+ *
+ * </ul>
+ */
+class NonPartneredSmallParcelDataOutput extends Model
+{
+
+    public function __construct($data = null)
+    {
+        $this->_fields = array(
+            'PackageList' => array('FieldValue' => null, 'FieldType' => NonPartneredSmallParcelPackageOutputList::className()),
+        );
+        parent::__construct($data);
+    }
+
+    /**
+     * Get the value of the PackageList property.
+     *
+     * @return NonPartneredSmallParcelPackageOutputList PackageList.
+     */
+    public function getPackageList()
+    {
+        return $this->_fields['PackageList']['FieldValue'];
+    }
+
+    /**
+     * Set the value of the PackageList property.
+     *
+     * @param NonPartneredSmallParcelPackageOutputList[] packageList
+     * @return this instance
+     */
+    public function setPackageList($value)
+    {
+        $this->_fields['PackageList']['FieldValue'] = $value;
+        return $this;
+    }
+
+    /**
+     * Check to see if PackageList is set.
+     *
+     * @return true if PackageList is set.
+     */
+    public function isSetPackageList()
+    {
+        return !is_null($this->_fields['PackageList']['FieldValue']);
+    }
+
+    /**
+     * Set the value of PackageList, return this.
+     *
+     * @param packageList
+     *             The new value to set.
+     *
+     * @return This instance.
+     */
+    public function withPackageList($value)
+    {
+        $this->setPackageList($value);
+        return $this;
+    }
+
+}
